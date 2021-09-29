cca3a4c860e9d5bdc4f340c6689c5b0c3a8f4665
HSW\leymir <mirko.leyh@hs-wismar.de>
2021-09-30 00:17:31 +0200

    [feature] added dynamic img width rendering
    calculated by imgcols value

--- Classes/DataProcessing/FilesProcessor.php
+++ Classes/DataProcessing/FilesProcessor.php
@@ -2,6 +2,8 @@
 
 /*
  * This file is part of the "headless" Extension for TYPO3 CMS.
+ * 
+ * edited for response_platform_theme
  *
  * For the full copyright and license information, please read the
  * LICENSE.md file that was distributed with this source code.
@@ -13,51 +15,15 @@ declare(strict_types=1);
 
 namespace Response\ResponsePlatformTheme\DataProcessing;
 
-// use FriendsOfTYPO3\Headless\DataProcessing;
-use FriendsOfTYPO3\Headless\Utility\FileUtility;
-use TYPO3\CMS\Core\Resource\FileInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
-use TYPO3\CMS\Frontend\Resource\FileCollector;
 
 /**
  * Class FilesProcessor
  */
-// class FilesProcessor extends \FriendsOfTYPO3\Headless\DataProcessing\FilesProcessor
-class FilesProcessor implements DataProcessorInterface
+class FilesProcessor extends \FriendsOfTYPO3\Headless\DataProcessing\FilesProcessor
+// class FilesProcessor implements DataProcessorInterface
 {
-    use DataProcessingTrait;
-
-    /**
-     * @var array
-     */
-    public $defaults = [
-        'as' => 'media',
-        'filesAs' => 'files',
-    ];
-
-    /**
-     * The content object renderer
-     *
-     * @var ContentObjectRenderer
-     */
-    protected $contentObjectRenderer;
-
-    /**
-     * The processor configuration
-     *
-     * @var array
-     */
-    protected $processorConfiguration;
-
-    /**
-     * The (filtered) media files to be used in the gallery
-     *
-     * @var FileInterface[]
-     */
-    protected $fileObjects = [];
-
+    
     /**
      * Process data for a gallery, for instance the CType "textmedia"
      *
@@ -76,18 +42,25 @@ class FilesProcessor implements DataProcessorInterface
         if (isset($processorConfiguration['if.']) && !$cObj->checkIf($processorConfiguration['if.'])) {
             return $processedData;
         }
+        
+        /* calculating target width ... */
+        $maxWidth = $processorConfiguration['processingConfiguration.']['maxWidth'] ?? null;
+        $imagecols = $processedData['data']['imagecols'] ?? null;
+        $columnSpacing = $processorConfiguration['processingConfiguration.']['columnSpacing'] ?? 0;
+        $tWidth = null;
+        if ($maxWidth) {
+            if ($imagecols && intval($imagecols) > 1) {
+                $tWidth = intval( ( $maxWidth - $columnSpacing * ($imagecols - 1) ) / $imagecols );
+            } else {
+                $tWidth = intval($maxWidth);
+            }
+        }
 
         $dimensions = [];
 
         if (isset($processorConfiguration['processingConfiguration.'])) {
             $dimensions = [
-                'width' => $processorConfiguration['processingConfiguration.']['width'] ?? null,
-                'height' => $processorConfiguration['processingConfiguration.']['height'] ?? null,
-                'minWidth' => $processorConfiguration['processingConfiguration.']['minWidth'] ?? null,
-                'minHeight' => $processorConfiguration['processingConfiguration.']['minHeight'] ?? null,
-                // 'maxWidth' => $processorConfiguration['processingConfiguration.']['maxWidth'] ?? null,
-                'maxWidth' => 1234,
-                'maxHeight' => $processorConfiguration['processingConfiguration.']['maxHeight'] ?? null,
+                'maxWidth' => $tWidth,
             ];
         }
 
@@ -106,137 +79,4 @@ class FilesProcessor implements DataProcessorInterface
         return $this->removeDataIfnotAppendInConfiguration($processorConfiguration, $processedData);
     }
 
-    /**
-     * @return array
-     */
-    protected function fetchData(): array
-    {
-        /** @var FileCollector $fileCollector */
-        $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
-
-        if (!empty($this->processorConfiguration['references.'])) {
-            $referenceConfiguration = $this->processorConfiguration['references.'];
-            $relationField = $this->contentObjectRenderer->stdWrapValue('fieldName', $referenceConfiguration);
-
-            // If no reference fieldName is set, there's nothing to do
-            if (!empty($relationField)) {
-                // Fetch the references of the default element
-                $relationTable = $this->contentObjectRenderer->stdWrapValue(
-                    'table',
-                    $referenceConfiguration,
-                    $this->contentObjectRenderer->getCurrentTable()
-                );
-                if (!empty($relationTable)) {
-                    $fileCollector->addFilesFromRelation(
-                        $relationTable,
-                        $relationField,
-                        $this->contentObjectRenderer->data
-                    );
-                }
-            }
-        }
-
-        $files = $this->contentObjectRenderer->stdWrapValue('files', $this->processorConfiguration);
-        if ($files) {
-            $files = GeneralUtility::intExplode(',', $files, true);
-            $fileCollector->addFiles($files);
-        }
-
-        $collections = $this->contentObjectRenderer->stdWrapValue('collections', $this->processorConfiguration);
-        if (!empty($collections)) {
-            $collections = GeneralUtility::trimExplode(',', $collections, true);
-            $fileCollector->addFilesFromFileCollections($collections);
-        }
-
-        $folders = $this->contentObjectRenderer->stdWrapValue('folders', $this->processorConfiguration);
-        if (!empty($folders)) {
-            $folders = GeneralUtility::trimExplode(',', $folders, true);
-            $fileCollector->addFilesFromFolders(
-                $folders,
-                !empty($this->processorConfiguration['folders.']['recursive'])
-            );
-        }
-
-        $sortingProperty = $this->contentObjectRenderer->stdWrapValue('sorting', $this->processorConfiguration);
-        if ($sortingProperty) {
-            $sortingDirection = $this->contentObjectRenderer->stdWrapValue(
-                'direction',
-                $this->processorConfiguration['sorting.'] ?? [],
-                'ascending'
-            );
-
-            $fileCollector->sort($sortingProperty, $sortingDirection);
-        }
-
-        return $fileCollector->getFiles();
-    }
-
-    /**
-     * @param array $dimensions
-     * @return array|null
-     */
-    protected function processFiles(array $dimensions = []): ?array
-    {
-        $data = [];
-        $cropVariant = $this->processorConfiguration['processingConfiguration.']['cropVariant'] ?? 'default';
-
-        foreach ($this->fileObjects as $fileObject) {
-            if (isset($this->processorConfiguration['processingConfiguration.']['autogenerate.'])) {
-                $file = $this->getFileUtility()->processFile($fileObject, $dimensions, $cropVariant);
-                $targetWidth = (int)($dimensions['width'] ?: $file['properties']['dimensions']['width']);
-                $targetHeight = (int)($dimensions['height'] ?: $file['properties']['dimensions']['height']);
-
-                if (isset($this->processorConfiguration['processingConfiguration.']['autogenerate.']['retina2x']) &&
-                    (int)$this->processorConfiguration['processingConfiguration.']['autogenerate.']['retina2x'] === 1 &&
-                    ($targetWidth || $targetHeight)) {
-                    $file['urlRetina'] = $this->getFileUtility()->processFile(
-                        $fileObject,
-                        array_merge(
-                            $dimensions,
-                            [
-                                'width' => $targetWidth * FileUtility::RETINA_RATIO,
-                                'height' => $targetHeight * FileUtility::RETINA_RATIO,
-                            ]
-                        ),
-                        $cropVariant
-                    )['publicUrl'];
-                }
-
-                if (isset($this->processorConfiguration['processingConfiguration.']['autogenerate.']['lqip']) &&
-                    (int)$this->processorConfiguration['processingConfiguration.']['autogenerate.']['lqip'] === 1 &&
-                    ($targetWidth || $targetHeight)) {
-                    $file['urlLqip'] = $this->getFileUtility()->processFile(
-                        $fileObject,
-                        array_merge(
-                            $dimensions,
-                            [
-                                'width' => $targetWidth * FileUtility::LQIP_RATIO,
-                                'height' => $targetHeight * FileUtility::LQIP_RATIO,
-                            ]
-                        ),
-                        $cropVariant
-                    )['publicUrl'];
-                }
-
-                $data[] = $file;
-            } else {
-                $data[] = $this->getFileUtility()->processFile($fileObject, $dimensions, $cropVariant);
-            }
-        }
-
-        if (isset($this->processorConfiguration['processingConfiguration.']['returnFlattenObject']) &&
-            (int)$this->processorConfiguration['processingConfiguration.']['returnFlattenObject'] === 1) {
-            return $data[0] ?? null;
-        }
-
-        return $data;
-    }
-
-    /**
-     * @return FileUtility
-     */
-    protected function getFileUtility(): FileUtility
-    {
-        return GeneralUtility::makeInstance(FileUtility::class);
-    }
 }
